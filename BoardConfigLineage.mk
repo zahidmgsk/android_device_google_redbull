95b22ae8e48c5d78fa5a7d49cb9d02baea8e7fa2
author zahidmgsk <zahidmgsk@gmail.com>
2023-12-07 08:08:09 +0000

    redbull: Init Lineage
    Signed-off-by: zahidmgsk <zahidmgsk@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2021 The LineageOS Project
-# Copyright (C) 2023 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -31,7 +30,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
 DEVICE_MANIFEST_FILE += device/google/redbull/lineage_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/evolution/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 
 # Needed for camera
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
@@ -43,8 +42,8 @@ AB_OTA_PARTITIONS += \
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # SELinux
-BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-evolution/dynamic
-BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-evolution/vendor
+BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-lineage/dynamic
+BOARD_SEPOLICY_DIRS += device/google/redbull/sepolicy-lineage/vendor
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
